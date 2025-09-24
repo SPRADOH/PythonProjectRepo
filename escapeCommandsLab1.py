519450429b0801ea7e4d8afa91806287870d4dcd
SPRADOH <felixakinloye0612@gmail.com>
2025-09-24 15:57:45 +0300

    escape sequences

---
+++ escapeCommandsLab1.py
@@ -0,0 +1,6 @@
+ESC = '\X'
+
+print("Hello World\nIt's me") # \n is used to go to a new line
+print("Hello World\tIt's me") # \t is used to give  a tab space
+print("Hello World\r5") # \r is used to replace the first character of the string with the next character
+print("Hello World\b5") # \b is used to remove the character before it
