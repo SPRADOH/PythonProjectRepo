5f845c623e9f1392bc3adc07bb16a09c85cd90b9
SPRADOH <felixakinloye0612@gmail.com>
2025-11-19 04:44:17 +0300

    corrected errors

--- LAB 4/main.py
+++ LAB 4/main.py
@@ -1,53 +1,53 @@
-MAX_VOLUME = 8
+MAX_SIZE = 8
 
 items = {
-    'r': {'price': 25, 'volume': 3},
-    'p': {'price': 15, 'volume': 2},
-    'a': {'price': 15, 'volume': 2},
-    'm': {'price': 20, 'volume': 2},
-    'i': {'price': 5, 'volume': 1},
-    'k': {'price': 15, 'volume': 1},
-    'x': {'price': 20, 'volume': 3},
-    't': {'price': 25, 'volume': 1},
-    'f': {'price': 15, 'volume': 1},
-    'd': {'price': 10, 'volume': 1},
-    's': {'price': 20, 'volume': 2},
-    'c': {'price': 20, 'volume': 2},
+    'r': {'points': 25, 'size': 3},
+    'p': {'points': 15, 'size': 2},
+    'a': {'points': 15, 'size': 2},
+    'm': {'points': 20, 'size': 2},
+    'i': {'points': 5,  'size': 1},
+    'k': {'points': 15, 'size': 1},
+    'x': {'points': 20, 'size': 3},
+    't': {'points': 25, 'size': 1},
+    'f': {'points': 15, 'size': 1},
+    'd': {'points': 10, 'size': 1},
+    's': {'points': 20, 'size': 2},
+    'c': {'points': 20, 'size': 2},
 }
 
 START_POINTS = 15
 
 
-def knapsack_dp(items, max_volume=MAX_VOLUME):
+def knapsack_dp(items, max_size=MAX_SIZE):
     item_list = list(items.keys())
     n = len(item_list)
     
-    total_all_points = sum(items[item]['price'] for item in items)
+    total_all_points = sum(items[item]['points'] for item in items)
     
-    table = [[0 for _ in range(max_volume + 1)] for _ in range(n + 1)]
+    table = [[0 for _ in range(max_size + 1)] for _ in range(n + 1)]
     
     for i in range(1, n + 1):
         item = item_list[i - 1]
-        price = items[item]['price']
-        volume = items[item]['volume']
+        points = items[item]['points']
+        size = items[item]['size']
         
-        for w in range(1, max_volume + 1):
-            if volume > w:
+        for w in range(1, max_size + 1):
+            if size > w:
                 table[i][w] = table[i - 1][w]
             else:
                 table[i][w] = max(table[i - 1][w], 
-                                 table[i - 1][w - volume] + price)
+                                 table[i - 1][w - size] + points)
     
-    w = max_volume
+    w = max_size
     selected_items = []
     
     for i in range(n, 0, -1):
         if table[i][w] != table[i - 1][w]:
             item = item_list[i - 1]
             selected_items.append(item)
-            w -= items[item]['volume']
+            w -= items[item]['size']
     
-    selected_points = table[n][max_volume]
+    selected_points = table[n][max_size]
     unselected_points = total_all_points - selected_points
     total_value = START_POINTS + selected_points - unselected_points
     
@@ -59,8 +59,8 @@ def display_inventory(selected_items, items):
     pos = 0
     
     for item in selected_items:
-        volume = items[item]['volume']
-        for _ in range(volume):
+        size = items[item]['size']
+        for _ in range(size):
             if pos < 8:
                 row = pos // 4
                 col = pos % 4
